91c9f175d25cfa100ec12f42bc2b6233c6a17645
Gage <gage.copelin@gmail.com>
2020-10-02 23:11:19 -0400

    set up bedroom

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    public GameObject interactionButton;
+    public GameObject objectToSetActive;
+    private bool isInTrigger;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isInTrigger)
+        {
+            objectToSetActive.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            interactionButton.SetActive(true);
+            isInTrigger = true;
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            interactionButton.SetActive(false);
+            isInTrigger = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    private float moveInputX;
+    private float moveInputY;
+
+    private bool talking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(!talking)
+        {
+            moveInputX = Input.GetAxis("Horizontal");
+            moveInputY = Input.GetAxis("Vertical");
+            Vector2 inputVector = new Vector2(moveInputX * speed, moveInputY * speed);
+            inputVector = Vector2.ClampMagnitude(inputVector, speed);
+
+            rb.velocity = inputVector;
+        }
+    }
+
+    public void InDialouge()
+    {
+        if(talking)
+        {
+            talking = false;
+        }
+        else
+        {
+            talking = true;
+        }
+
+    }
+}
