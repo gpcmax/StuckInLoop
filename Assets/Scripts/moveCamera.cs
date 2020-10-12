c37235b9b2ab0131f831db3e511613478e0e1315
Gage <gage.copelin@gmail.com>
2020-10-11 21:24:51 -0400

    all art is done
    base game is done, now is code clean up and sounds

---
+++ Assets/Scripts/moveCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class moveCamera : MonoBehaviour
+{
+    public float speed = 1;
+
+    void FixedUpdate()
+    {
+        transform.Translate((transform.right * speed) * Time.deltaTime);
+    }
+}
